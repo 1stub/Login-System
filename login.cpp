750a2e8ac691ee073b7c12b5c1734f5c3859c79e
1stub <stubbiegaming@gmail.com>
2023-03-07 12:38:34 -0500

    fixed password login

--- login.cpp
+++ login.cpp
@@ -4,58 +4,6 @@
 #include <vector>
 #include "login.h"
 
-/*void loginUser()
-{
-	std::ifstream usernames; usernames.open("usernames.txt");
-	if (usernames.is_open())
-	{
-		std::cout << "Enter your Username: " << std::endl;
-		std::string usernameLogin;
-		std::cin >> usernameLogin;
-		std::string line;
-		bool found = false;
-		int count = 0;
-		int lineNum = 0;
-		while( std::getline( usernames , line) && !found)
-		{ 
-			count++;
-			if (line.find(usernameLogin) != std::string::npos)
-			{
-				found = true;
-			}
-		}
-		if (!found)
-		{
-			std::cout << "Username not found. Please re-enter." << std::endl;
-		}
-	}
-	std::ifstream passwords; passwords.open("passwords.txt");
-	if (passwords.is_open())
-	{
-			std::cout << "Enter your Password: " << std::endl;
-			std::string passwordLogin;
-			std::cin >> passwordLogin;
-			std::string line;
-			bool found = false;
-			while (std::getline(usernames, line) && !found)
-			{
-				if (line.find(passwordLogin) != std::string::npos)
-				{
-					found = true;
-				}
-			}
-			if (!found)
-			{
-				std::cout << "Password not found. Please re-enter." << std::endl;
-			}
-			if (found == true)
-			{
-				std::cout << "Password Found! " << std::endl;
-				exit;
-			}
-		}
-}*/
-
 int findUsername()
 {
 	std::ifstream usernames("usernames.txt");
@@ -102,22 +50,34 @@ int findUsername()
 void findPassword(int count)
 {
 	std::ifstream passwords; passwords.open("passwords.txt");
+	std::vector<std::string>passwordList;
+
 	if (passwords.is_open())
+	{
+		std::string line;
+		while (std::getline(passwords, line))
+		{
+			passwordList.push_back(line);
+		}
+		passwords.close();
+	}
+	bool found = false;
+	while (!found)
 	{
 		std::cout << "Enter your Password: " << std::endl;
 		std::string passwordLogin;
 		std::cin >> passwordLogin;
-		std::string line;
-		int current_line = 0;
-
-		while (!passwords.eof())
+		for (const auto& password : passwordList)
 		{
-			current_line++;
-			std::getline(passwords, line);
-			if (current_line == count)
+			std::string vector_val = passwordList[count-1];
+			if (passwordLogin == vector_val)
 			{
-				std::cout << "Password Found!" << std::endl;
+				std::cout << "Those passwords match!" << std::endl;
 				break;
+			}
+			else
+			{
+				std::cout << "Password not found. Please re-enter." << std::endl;
 			}
 		}
 	}
